841a060161ad76feac0864906eb73ba2528fa545
Wajdan Ahmad <128275708+bhattiwajdan@users.noreply.github.com>
2023-04-17 10:43:30 +0500

    Just Starting...

---
+++ app/src/main/java/com/example/mycalc/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.mycalc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+    }
+}
